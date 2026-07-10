cb742fc936f94cad218efc9d3ed0e7f59f269453
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-10 22:02:44 +0300

    add group-price

---
+++ group-price.js
@@ -0,0 +1,9 @@
+function groupPrice(str) {
+    const regex = /([A-Z]{3})(\d+)\.(\d{2})/g;
+    const result = [];
+    let match;
+    while ((match = regex.exec(str)) !== null) {
+        result.push([match[0], match[2], match[3]]);
+    }
+    return result;
+}
