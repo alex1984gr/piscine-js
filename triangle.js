02a5c4496e0a45c8b7b216ad00e0fc6ba3318971
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-10 21:38:50 +0300

    feat: add triangle function

---
+++ triangle.js
@@ -0,0 +1,10 @@
+function triangle(char, height) {
+  let result = '';
+  for (let i = 1; i <= height; i++) {
+    for (let j = 0; j < i; j++) {
+      result += char;
+    }
+    if (i < height) result += '\n';
+  }
+  return result;
+}
