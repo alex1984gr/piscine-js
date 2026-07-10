958a87fda42585e3bca0141f5315d47ff88aada4
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-10 22:04:00 +0300

    fix group-price: match $ symbol

--- group-price.js
+++ group-price.js
@@ -1,5 +1,5 @@
 function groupPrice(str) {
-    const regex = /([A-Z]{3})(\d+)\.(\d{2})/g;
+    const regex = /(\$)(\d+)\.(\d{2})/g;
     const result = [];
     let match;
     while ((match = regex.exec(str)) !== null) {
