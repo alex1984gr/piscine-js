7c27922e1293e0968155075fd03096fb134d91de
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-10 21:40:29 +0300

    fix: pyramid spacing for multi-char strings

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,8 @@
 function pyramid(char, depth) {
   let result = '';
+  const charLen = char.length;
   for (let i = 0; i < depth; i++) {
-    const spaces = ' '.repeat(depth - i - 1);
+    const spaces = ' '.repeat((depth - i - 1) * charLen);
     const chars = char.repeat(2 * i + 1);
     result += spaces + chars;
     if (i < depth - 1) result += '\n';
