33dd446ca1855b57b5ae6fd95b219eff9b094cc2
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-10 22:12:24 +0300

    fix valid-ip: prevent double-matching IPs that have a port

--- valid-ip.js
+++ valid-ip.js
@@ -2,6 +2,6 @@ function findIP(str) {
     const octet = '(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])';
     const ip = `${octet}\\.${octet}\\.${octet}\\.${octet}`;
     const port = '(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[0-9]{1,4})';
-    const regex = new RegExp(`(?<![\\d.])${ip}(?::${port})?(?![\\d.])`, 'g');
+    const regex = new RegExp(`(?<![\\d.])${ip}:${port}(?![\\d.])|(?<![\\d.:])${ip}(?![\\d.:])`, 'g');
     return str.match(regex) || [];
 }
