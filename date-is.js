47edc9e5d355c15825f4b48b88193e03dddd6226
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-26 15:54:56 +0300

    fix date-is: accept number timestamps (Date.now()) in all functions

--- date-is.js
+++ date-is.js
@@ -1,19 +1,28 @@
+function toDate(date) {
+    if (date instanceof Date) return date;
+    if (typeof date === 'number') return new Date(date);
+    return date;
+}
+
 function isValid(date) {
-    return date instanceof Date && !isNaN(date.getTime());
+    const d = toDate(date);
+    return d instanceof Date && !isNaN(d.getTime());
 }
 
 function isAfter(date1, date2) {
-    return date1.getTime() > date2.getTime();
+    return toDate(date1).getTime() > toDate(date2).getTime();
 }
 
 function isBefore(date1, date2) {
-    return date1.getTime() < date2.getTime();
+    return toDate(date1).getTime() < toDate(date2).getTime();
 }
 
 function isFuture(date) {
-    return isValid(date) && date.getTime() > Date.now();
+    const d = toDate(date);
+    return isValid(d) && d.getTime() > Date.now();
 }
 
 function isPast(date) {
-    return isValid(date) && date.getTime() < Date.now();
+    const d = toDate(date);
+    return isValid(d) && d.getTime() < Date.now();
 }
