2a1e2994aefc9b05945a335ec282335a833daca9
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-26 19:33:52 +0300

    fix dayOfTheYear for years < 100

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,4 +1,6 @@
 const dayOfTheYear = (date) => {
-    const start = new Date(date.getFullYear(), 0, 1)
+    const start = new Date(date.getTime())
+    start.setMonth(0, 1)
+    start.setHours(0, 0, 0, 0)
     return Math.round((date - start) / 86400000) + 1
 }
