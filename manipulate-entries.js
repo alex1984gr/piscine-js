f9008eeaa97717c5915149823d1ff6eccf97d2c2
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-27 04:44:52 +0300

    add manipulate-entries

---
+++ manipulate-entries.js
@@ -0,0 +1,24 @@
+const filterEntries = (obj, fn) =>
+    Object.fromEntries(Object.entries(obj).filter(fn))
+
+const mapEntries = (obj, fn) =>
+    Object.fromEntries(Object.entries(obj).map(fn))
+
+const reduceEntries = (obj, fn, init) => {
+    const entries = Object.entries(obj)
+    return init !== undefined ? entries.reduce(fn, init) : entries.reduce(fn)
+}
+
+const totalCalories = (cart) =>
+    reduceEntries(cart, (acc, [name, grams]) => acc + nutritionDB[name].calories * grams / 100, 0)
+
+const lowCarbs = (cart) =>
+    filterEntries(cart, ([name, grams]) => nutritionDB[name].carbs * grams / 100 < 50)
+
+const cartTotal = (cart) =>
+    mapEntries(cart, ([name, grams]) => [
+        name,
+        mapEntries(nutritionDB[name], ([nutrient, per100]) =>
+            [nutrient, Math.round(per100 * grams / 100 * 10) / 10]
+        )
+    ])
