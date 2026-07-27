a937de824c639d6391ffaa8ab0bb6fb20083ad19
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-27 22:42:30 +0300

    fix manipulate-entries: restore filterEntries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -1,4 +1,6 @@
 const round1 = (n) => Math.round((n + Number.EPSILON) * 10) / 10
+
+const filterEntries = (obj, fn) =>
     Object.fromEntries(Object.entries(obj).filter(fn))
 
 const mapEntries = (obj, fn) =>
@@ -10,7 +12,7 @@ const reduceEntries = (obj, fn, init) => {
 }
 
 const totalCalories = (cart) =>
-    Math.round((reduceEntries(cart, (acc, [name, grams]) => acc + nutritionDB[name].calories * grams / 100, 0) + Number.EPSILON) * 10) / 10
+    round1(reduceEntries(cart, (acc, [name, grams]) => acc + nutritionDB[name].calories * grams / 100, 0))
 
 const lowCarbs = (cart) =>
     filterEntries(cart, ([name, grams]) => nutritionDB[name].carbs * grams / 100 < 50)
