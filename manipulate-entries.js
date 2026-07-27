2d49c7afb6ee9fe4f5e2ee217b0aa44be24aa417
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-27 22:45:04 +0300

    fix cartTotal: remove rounding, return raw values

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -21,6 +21,6 @@ const cartTotal = (cart) =>
     mapEntries(cart, ([name, grams]) => [
         name,
         mapEntries(nutritionDB[name], ([nutrient, per100]) =>
-            [nutrient, round1(per100 * grams / 100)]
+            [nutrient, per100 * grams / 100]
         )
     ])
