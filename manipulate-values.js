0ba427e2592f68452f712acf19dab915f07d7cae
agaliand <alexandrosgaliandras@yahoo.com>
2026-07-27 04:33:05 +0300

    fix reduceValues: only pass init when provided

--- manipulate-values.js
+++ manipulate-values.js
@@ -4,5 +4,7 @@ const filterValues = (obj, fn) =>
 const mapValues = (obj, fn) =>
     Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, fn(v)]))
 
-const reduceValues = (obj, fn, init) =>
-    Object.values(obj).reduce(fn, init)
+const reduceValues = (obj, fn, init) => {
+    const vals = Object.values(obj)
+    return init !== undefined ? vals.reduce(fn, init) : vals.reduce(fn)
+}
